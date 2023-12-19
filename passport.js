f9a2dff744759347f8e417f6cd41a946973968c0
Ridwan <aladebaruwa@gmail.com>
2023-12-19 11:07:22 +0100

    add

--- passport.js
+++ passport.js
@@ -2,7 +2,7 @@ window.passport = new window.immutable.passport.Passport({
     baseConfig: new window.immutable.config.ImmutableConfiguration({
       environment: window.immutable.config.Environment.SANDBOX,
     }),
-    clientId: 'enter-client-id',
+    clientId: '2EMwHUNVzJTJZEErVwtJqQAUcR7Lu7f5',
     redirectUri: 'redirect-url',
     logoutRedirectUri: 'logout-url',
     audience: 'platform_api',
