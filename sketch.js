a206a7df7194672551e44b636521e79a581e3585
Ridwan <aladebaruwa@gmail.com>
2023-12-19 11:46:06 +0100

    add

---
+++ sketch.js
@@ -0,0 +1,12 @@
+let alienImage; 
+let invaders;
+let shooterImage;
+let player;
+let allDebris = [];
+let gameOver = false;
+let canvas;
+let canvasEl;
+let loading = 10;
+let loadingPlus = true;
+let resumeButton;
+let upgradedShooterImage;
