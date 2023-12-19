1de3190df73f04de7717d1509fb22eb9e83af672
Ridwan <aladebaruwa@gmail.com>
2023-12-19 11:36:00 +0100

    add

--- Player.js
+++ Player.js
@@ -41,8 +41,44 @@ class Player {
         this.image = loadImage('playerv2.png');
         this.maxBullets = 4;
     }
-// game state
 
+
+// game state
+ update() {
+        if (this.gamePaused) return;
+        if (this.isMovingRight && this.x < width -40) {
+            this.x += 1;
+        } else if (this.isMovingLeft && this.x > 0) {
+            this.x -= 1;
+        }
+        
+        if(this.isMovingUp && this.y > 0){
+            this.y -= 1;
+        } else if(this.isMovingDown && this.y < height - 30){
+            this.y += 1;
+        }
+        this.updateBullets();
+    }
+
+    updateBullets() {
+        for (let i = this.bullets.length - 1; i >= 0; i--) {
+            this.bullets[i].update();
+            if (this.hasHitAlien(this.bullets[i])) {
+                this.bullets.splice(i, 1);
+                this.score += 10;
+                break;
+            } else if (this.bullets[i].isOffScreen()) {
+                this.bullets.splice(i, 1);
+                break;
+            }
+        }
+    }
+
+    pauseGame(tokenId) {
+        this.gamePaused = true;
+        this.showNft(tokenId);
+    }
+    
 // movement methods
 
 // drawing methods
